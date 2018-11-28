241e1f34ed994684de331fd5b5d0fb9fd3684d6a
marysialeszko <marialeszko@MacBook-Maria.local>
2018-11-28 14:32:54 +0100

    Fix st louis album

--- db/migrate/20181128141700_add_stlouis_album.rb
+++ db/migrate/20181128141700_add_stlouis_album.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
-class AddStLouisAlbum < ActiveRecord::Migration[5.0]
+class AddStlouisAlbum < ActiveRecord::Migration[5.0]
   def up
     Album.create(title: "18.11.16-18 ST LOUIS", start_date: "2018-11-16", end_date: "2018-11-18", logo_url: "albums/stlouis.jpg", album_url: "https://photos.app.goo.gl/FjurujNc5sTMHSL67", text: "Weekend w cieniu łuku w St Louis.", created_at: "2018-11-28 14:17:46", updated_at: "2018-11-28 14:17:46")
   end
